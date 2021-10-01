930035cbb71d562eb27496bfb564cac7f6af7795
Xenovka <wendyanto18@gmail.com>
2021-10-02 00:21:40 +0700

    edit feature finished

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,92 @@
-let JSON_DATA;
+const MODAL = new bootstrap.Modal(document.getElementById("editModal"), {
+  backdrop: "static",
+  keyboard: false
+});
 
+let JSON_DATA;
+let EDIT_VALUE;
+
+function onSubmitMenu() {
+  const inputForm = $("#form-input");
+  const tableMenu = $("#table-menu tbody");
+
+  inputForm.on("submit", async (e) => {
+    e.preventDefault();
+
+    const data = JSON_DATA;
+    const inputElement = $("#input-menu");
+
+    const isExist = data.filter(
+      (food) => inputElement.val().trim() === food.name
+    );
+
+    if (isExist.length) {
+      alert("Error: Food you want to create was already exists in the table!");
+      return;
+    }
+
+    data.push({
+      id: data.length + 1,
+      name: inputElement.val(),
+      category: generateCategory()
+    });
+
+    alert("Success: Menu successfully added!");
+
+    inputElement[0].value = "";
+
+    tableMenu.append(appendRow(data.length, data[data.length - 1].name));
+  });
+}
+
+function onDeleteMenu(value) {
+  const tableMenu = $("#table-menu tbody");
+  JSON_DATA = JSON_DATA.filter((food) => food.name !== value);
+
+  alert("Success: Data successfully deleted!");
+
+  tableMenu.html("");
+
+  JSON_DATA.forEach((food, idx) => {
+    tableMenu.append(appendRow(idx + 1, food.name));
+  });
+}
+
+function onEditMenu() {
+  const inputError = $("#input-error");
+  const modalForm = $("#edit-input");
+  const modalValue = modalForm.val();
+  const tableMenu = $("#table-menu tbody");
+
+  inputError.empty();
+
+  if (!modalValue.length) {
+    inputError.append("Error: Input can't be empty!");
+    return;
+  }
+
+  for (let i = 0; i < JSON_DATA.length; i++) {
+    if (modalValue === JSON_DATA[i].name) {
+      inputError.append("Error: Input can't be same like the existing menu!");
+      return;
+    }
+  }
+
+  JSON_DATA.forEach((food) => {
+    if (food.name === EDIT_VALUE) {
+      food.name = modalValue;
+      alert("Success: Data edited successfully");
+      MODAL.hide();
+
+      tableMenu.html("");
+      JSON_DATA.forEach((food, idx) => {
+        tableMenu.append(appendRow(idx + 1, food.name));
+      });
+    }
+  });
+}
+
+// * Helper Functions
 async function loadDefaultData() {
   const response = await fetch("/assets/javascript/food.json");
   JSON_DATA = await response.json();
@@ -9,34 +96,7 @@ async function loadDefaultData() {
   const data = JSON_DATA;
 
   for (let counter = 0; counter < 3; counter++) {
-    tableMenu.append(`
-      <tr>
-        <td class="number">${counter + 1}</td>
-        <td class="name">${data[counter].name}</td>
-        <td class="action">
-          <a id="edit" class="btn btn-primary btn-table" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@fat" href="#">
-            <img
-              class="edit-icon"
-              src="/assets/images/edit-icon.png"
-              alt="edit icon button"
-  
-            />
-            <span class="btn-text">Edit</span>
-          </a>
-
-          <a id="delete" class="btn btn-primary btn-table" href="#" onClick="onDeleteMenu('${
-            data[counter].name
-          }')">
-            <img
-              class="delete-icon"
-              src="/assets/images/delete-icon.png"
-              alt="edit icon button"
-            />
-            <span class="btn-text">Delete</span>
-          </a>
-        </td>
-      </tr>
-    `);
+    tableMenu.append(appendRow(counter + 1, data[counter].name));
   }
 }
 
@@ -53,104 +113,44 @@ function generateCategory() {
   }
 }
 
-function onSubmitMenu() {
-  const inputForm = $("#form-input");
-  const tableMenu = $("#table-menu tbody");
-
-  inputForm.on("submit", async (e) => {
-    e.preventDefault();
-
-    const data = JSON_DATA;
-    const inputElement = $("#input-menu");
-
-    const isExist = data.filter(
-      (food) => inputElement.get(0).value.trim() === food.name
-    );
-
-    if (isExist.length) {
-      alert("Error: Food you want to create was already exists in the table!");
-      return;
-    }
-
-    data.push({
-      id: data.length + 1,
-      name: inputElement.get(0).value,
-      category: generateCategory(),
-    });
-
-    alert("Success: Menu successfully added!");
-
-    inputElement[0].value = "";
-
-    tableMenu.append(`
-      <tr>
-        <td class="number">${data.length}</td>
-        <td class="name">${data[data.length - 1].name}</td>
-        <td class="action">
-          <a id="edit" class="btn btn-primary btn-table" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@fat" href="#">
-            <img
-              class="edit-icon"
-              src="/assets/images/edit-icon.png"
-              alt="edit icon button"
-            />
-            <span class="btn-text">Edit</span>
-          </a>
-
-          <a id="delete" class="btn btn-primary btn-table" href="#" onClick="onDeleteMenu('${
-            data[data.length - 1].name
-          }')">
-            <img
-              class="delete-icon"
-              src="/assets/images/delete-icon.png"
-              alt="edit icon button"
-            />
-            <span class="btn-text">Delete</span>
-          </a>
-        </td>
-      </tr>
-    `);
-  });
+function onEditSubmit() {
+  $("#modal-form").on("submit", (e) => e.preventDefault());
 }
 
-function onDeleteMenu(value) {
-  const tableMenu = $("#table-menu tbody");
-  JSON_DATA = JSON_DATA.filter((food) => food.name !== value);
-
-  alert("Success: Data successfully deleted!");
-
-  tableMenu.html("");
-
-  JSON_DATA.forEach((food, idx) => {
-    tableMenu.append(`
-      <tr>
-        <td class="number">${idx + 1}</td>
-        <td class="name">${food.name}</td>
-        <td class="action">
-          <a id="edit" class="btn btn-primary btn-table" href="#">
-            <img
-              class="edit-icon"
-              src="/assets/images/edit-icon.png"
-              alt="edit icon button"
-            />
-            <span class="btn-text">Edit</span>
-          </a>
-
-          <a id="delete" class="btn btn-primary btn-table" href="#" onClick="onDeleteMenu('${
-            food.name
-          }')">
-            <img
-              class="delete-icon"
-              src="/assets/images/delete-icon.png"
-              alt="edit icon button"
-            />
-            <span class="btn-text">Delete</span>
-          </a>
-        </td>
-      </tr>
-    `);
-  });
+function getEditTarget(value) {
+  document.getElementById("edit-input").value = "";
+  $("#input-error").empty();
+  return (EDIT_VALUE = value);
 }
 
-function onEditMenu() {}
+// * Resusable Code Functions
+function appendRow(counter, data) {
+  return `
+    <tr>
+      <td class="number">${counter}</td>
+      <td class="name">${data}</td>
+      <td class="action">
+        <a id="edit" class="btn btn-primary btn-table" data-bs-toggle="modal" onclick="getEditTarget('${data}')" data-bs-target="#editModal" href="#">
+          <img
+            class="edit-icon"
+            src="/assets/images/edit-icon.png"
+            alt="edit icon button"
+          />
+          <span class="btn-text">Edit</span>
+        </a>
+
+        <a id="delete" class="btn btn-primary btn-table" href="#" onClick="onDeleteMenu('${data}')">
+          <img
+            class="delete-icon"
+            src="/assets/images/delete-icon.png"
+            alt="edit icon button"
+          />
+          <span class="btn-text">Delete</span>
+        </a>
+      </td>
+    </tr>
+  `;
+}
 
 onSubmitMenu();
+onEditSubmit();
